f169f9aa748431a4edac4e93dc84a041a774e120
Moealsir <mohamedwdalsir@gmail.com>
2024-06-25 23:53:59 +0300

    7-getBudgetObject.js

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,12 @@
-#!/usr/bin/node
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
     income,
