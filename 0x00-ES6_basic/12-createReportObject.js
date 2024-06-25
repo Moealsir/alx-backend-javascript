396bc16d2c1301bb4a7f3ce81a5dbfdae4e18342
Moealsir <mohamedwdalsir@gmail.com>
2024-06-26 01:08:58 +0300

    12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,11 +2,10 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: {
-      ...employeesList
+      ...employeesList,
     },
     getNumberOfDepartments(employeesList) {
       return Object.keys(employeesList).length;
-    }
+    },
   };
 }
-
