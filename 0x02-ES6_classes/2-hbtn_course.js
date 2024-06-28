1876b4d6dc09786f2bca38abd9194297ce434c45
Moealsir <mohamedwdalsir@gmail.com>
2024-06-28 05:01:42 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,44 @@
 #!/usr/bin/node
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+  if (typeof name === 'string') {
+    this._name = name;
+  }
+  throw new TypeError('Name must be a string');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    }
+    throw new TypeError('Length must be a number');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    if (!(students instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (students.every((student) => typeof student === 'string')) {
+      this._students = students;
+    }
+    throw new TypeError('Students must be an array of strings');
+  }
+}
