a649cac257ddcbe12346c2a0cfc295ef5d7ea932
Moealsir <mohamedwdalsir@gmail.com>
2024-07-02 23:19:54 +0300

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
-    return students.reduce((prevStudent, currStudent) => prevStudent.id || prevStudent + currStudent.id, 0); 
+    return students.reduce((prevStudent, currStudent) =>
+      prevStudent.id || prevStudent + currStudent.id, 0); 
   }
 }
